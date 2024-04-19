b0258ec0b17bce4c3175c71fed8b28f0bdbcb180
dilmurat <dilmurat.user@gmail.com>
2024-04-19 23:26:37 +0500

    initial commit

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = \App\User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
